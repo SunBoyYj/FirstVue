52fb22d9cee57212cf185228d6d6c437e0fe41b2
jeremy <18266617307@163.com>
2017-12-15 21:31:12 +0800

    上次忘了上传到muster路线

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '../components/page/index'
+import '../assets/style/reset.css'
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'index',
+      component: Index
     }
   ]
 })
