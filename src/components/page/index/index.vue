406dc07bc538431aa9eed9c94146320ca8551a58
jeremy <18266617307@163.com>
2017-12-16 21:43:45 +0800

    头部导航

--- src/components/page/index/index.vue
+++ src/components/page/index/index.vue
@@ -1,23 +1,33 @@
 <template>
   <div>
     <header class="header">
-      <div class="back"><i class="icon"></i></div>
-      <div class="search"></div>
-      <div class="city">城市</div>
+      <div class="back iconfont">&#xe624;</div>
+      <div class="search">
+        <i class=" fdj iconfont">&#xe60b;</i>
+        <a href="#" class="prompt">输入城市/景点/游玩主题</a>
+      </div>
+      <div class="city"><a href="#" class="city-content">城市</a><i class="iconfont">&#xe62d;</i></div>
     </header>
 
     <swiper :options="swiperOption">
-      <swiper-slide>
+      <swiper-slide v-for="item in swiperInfo" :key="item.id">
         <div class="swiper-img-con">
-          <img  class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1609/15/630b82d932a3c402.jpg_640x200_862e836b.jpg"/>
-        </div>
-      </swiper-slide>
-      <swiper-slide>
-        <div class="swiper-img-con">
-          <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1712/5c/723bbb93e9683f02.jpg_640x200_7d38c6ad.jpg"/>
+          <img  class="swiper-img" :src="item.imgUrl"/>
         </div>
       </swiper-slide>
       <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+
+    <swiper>
+      <swiper-slide v-for="(pageInfo, index) in pages" :key="index">
+        <div class="icon-wrapper">
+          <div v-for="item in pageInfo" :key="item.id" class="icon-item">
+            <div class="icon-img-con">
+              <img  class="icon-img" :src="item.imgUrl"/>
+            </div>
+          </div>
+        </div>
+      </swiper-slide>
     </swiper>
 
   </div>
@@ -26,13 +36,47 @@
 <script>
 export default {
   name: 'Index',
+
   data () {
     return {
+      swiperInfo: [],
+      iconInfo: [],
       swiperOption: {
-        autoplay: 1000,
-        direction: 'horizontal'
+        autoplay: 10000,
+        pagination: '.swiper-pagination',
+        loop: true
       }
     }
+  },
+
+  computed: {
+    pages () {
+      const pages = []
+      this.iconInfo.forEach((value, index) => {
+        let page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(value)
+      })
+      return pages
+    }
+  },
+
+  methods: {
+    handleSwiperDate () {
+      this.$http.get('/static/index.json')
+          .then(this.handleSwiperDateSucc.bind(this))
+    },
+
+    handleSwiperDateSucc (res) {
+      this.swiperInfo = res.body.data.swiper
+      this.iconInfo = res.body.data.icons
+    }
+  },
+
+  created: function () {
+    this.handleSwiperDate()
   }
 }
 </script>
@@ -46,22 +90,64 @@ export default {
   .back {
     width: .64rem;
     line-height: .86rem;
+    text-align: center;
   }
   .search {
     flex: 1;
     margin: .14rem .18rem;
     background: #fff;
     border-radius: .1rem;
+    padding-top: 0.16rem;
+  }
+  .fdj{
+    color: #e4e7ea;
+    float: left;
+    margin-left: 0.3rem;
+    margin-right: 0.24rem;
+  }
+  .prompt{
+    font-size: 0.2rem;
+    color: #e4e7ea;  
   }
   .city {
     width: 1.14rem;
     line-height: .86rem;
     text-align: center;
   }
+  .city-content{
+    color: #fff;
+    display: block;
+    float: left;
+    width: 60%;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
   .swiper-img-con {
+    overflow: hidden;
     width: 100%;
+    height: 0;
+    padding-bottom: 31.25%;
+    /*防止抖屏，将高度设置为零，加一个padding-bottom 比例为高度/宽度*/
   }
   .swiper-img {
+    width: 100%;
+  }
+  .icon-wrapper {
+
+  }
+  .icon-item {
+    box-sizing: border-box;
+    float: left;
+    width: 25%;
+    padding: .4rem;
+  }
+  .icon-img-con {
+    width: 100%;
+    height: 0;
+    padding-bottom: 100%;
+  }
+  .icon-img {
     width: 100%;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'swiper/dist/css/swiper.css'
+import './assets/style/iconfont/iconfont.css'
 import VueSwiper from 'vue-awesome-swiper'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 Vue.use(VueSwiper)
+Vue.use(VueResource)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
